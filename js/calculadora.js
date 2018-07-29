78f23bdd9e282b5cbfdf8bd6b2b9a66155d12d3c
cbruna01 <c.bruna01@ufromail.cl>
2018-07-28 22:48:45 -0400

    agregada tabla animales

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
+
 window.onload = inicializar;
 
-function inicializar() {
-  if (localStorage.getItem('admin') == 'false' && localStorage.getItem('operario') == 'false') {
-      window.location.href = "index.html";
-  }
-
-  if (localStorage.getItem('admin') == 'true') {
-      document.getElementById('btn-usuarios').style.display= "inline";
-  }
-
-  var botonCerrarSesion = document.getElementById("btn-cerrarSesion");
-  botonCerrarSesion.addEventListener("click" , logout, false);
-=======
 //------------------------------- WINDOW ON LOAD ----------------------------------//
 
 if (localStorage.getItem('admin') == null && localStorage.getItem('operario') == null) {
@@ -30,16 +18,14 @@ var botonCerrarSesion;
 
 //------------------------------- INICIALIZAR -----------------------------------//
 
-function inicializar() {
-  
+function inicializar() { 
+
   if (localStorage.getItem('admin') == 'true') {
-    document.getElementById('btn-usuarios').style.display= "inline";
+      document.getElementById('btn-usuarios').style.display= "inline";
   }
 
-  botonCerrarSesion = document.getElementById("btn-cerrarSesion");    
+  var botonCerrarSesion = document.getElementById("btn-cerrarSesion");
   botonCerrarSesion.addEventListener("click" , logout, false);
-    
->>>>>>> d963d60ca6bf3bd600af94f9b3577519ee3b9a67
 }
 
 //----------------------------- CALCULAR ------------------------------//
@@ -47,10 +33,15 @@ function calcular() {
 	var dosis = document.getElementById("dosis").value;
 	var area = document.getElementById('area').value;
 
-	var litros = (dosis*area)/1000;
-	var cm3 = litros*1000;
-	document.getElementById('litros').value = litros;
-	document.getElementById('cm3').value = cm3;
+  if (dosis == 0 || area == 0 ) {
+    alertify.set('notifier','position', 'top-center');
+    alertify.error('Todos los campos son obligatorios', 3);
+  } else {
+    var litros = (dosis*area)/1000;
+    var cm3 = litros*1000;
+    document.getElementById('litros').value = litros;
+    document.getElementById('cm3').value = cm3;
+  }	
 }
 
 //----------------------------- LOGOUT --------------------------------//
